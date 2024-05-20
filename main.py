4433f9804c8d4a2fb35ab905db8816ec68a95492
Michael Olmstead <molmstead4876@xmail.dixietech.edu>
2024-05-19 18:28:51 -0700

    Main with implemented api

---
+++ main.py
@@ -0,0 +1,65 @@
+import uuid
+
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+from schemas import (
+    Movie,
+    CreateMovieRequest,
+    CreateMovieResponse,
+    UpdateMovieRequest,
+    UpdateMovieResponse,
+    DeleteMovieResponse,
+)
+
+
+movies: list[Movie] = [
+    Movie(movie_id=uuid.uuid4(), name="Spider-Man", year=2002),
+    Movie(movie_id=uuid.uuid4(), name="Thor: Ragnarok", year=2017),
+    Movie(movie_id=uuid.uuid4(), name="Iron Man", year=2008),
+]
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/movies")
+async def get_movies() -> list[Movie]:
+    return movies
+
+@app.post("/movies")
+async def create_movie(new_movie: CreateMovieRequest) -> CreateMovieResponse:
+    
+    movie_id = uuid.uuid4()
+    movie = Movie(movie_id=movie_id, name=new_movie.name, year=new_movie.year)
+    movies.append(movie)
+    return CreateMovieResponse(id=movie_id)
+
+@app.put("/movies/{movie_id}")
+async def update_movie(movie_id: uuid.UUID, updated_movie: UpdateMovieRequest) -> UpdateMovieResponse:
+    
+    for movie in movies:
+        if movie.movie_id == movie_id:
+            movie.name = updated_movie.name
+            movie.year = updated_movie.year
+            return UpdateMovieResponse(success=True)
+    
+    raise HTTPException(status_code=404, detail="Movie not found")
+
+@app.delete("/movies/{movie_id}")
+async def delete_movie(movie_id: uuid.UUID) -> DeleteMovieResponse:
+    
+    global movies
+    for movie in movies:
+        if movie.movie_id == movie_id:
+            movies = [m for m in movies if m.movie_id != movie_id]
+            return DeleteMovieResponse(success=True)
+    
+    raise HTTPException(status_code=404, detail="Movie not found")
